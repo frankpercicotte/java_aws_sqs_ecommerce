e73853236f22996d19beb800d49fa3299b92424b
Franklin Percicotte <43394686+frankpercicotte@users.noreply.github.com>
2022-05-01 21:58:49 -0300

    refatorando sqsservice

--- src/main/java/br/com/sqs_ecommerce/App.java
+++ src/main/java/br/com/sqs_ecommerce/App.java
@@ -1,25 +1,16 @@
 package br.com.sqs_ecommerce;
 
-import br.com.sqs_ecommerce.dto.InfMessage;
 import br.com.sqs_ecommerce.services.SQSService;
-import java.time.LocalDateTime;
 
-public class App 
-{
-    public static void main( String[] args ) throws InterruptedException
-    {
-        System.out.println("Sistema de Simulação de ecommerce!");        
+public class App {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Sistema de Simulação de ecommerce!");
         System.out.println("Colocando pedido no Sistema:");
-        
-        // TODO - logica de colocar novos pedidos
-        // InfMessage infMessage = new InfMessage("pedidos","transacao",123);
-        // LocalDateTime now = LocalDateTime.now();
-        // System.out.println("["+now+"] Pedido# " + infMessage);
-        // SQSService.sendMessage(sqsClient, queueUrl, message);
 
-        while(true){
-            SQSService.messageReader();        
+        // TODO - fase 2, criar logica de colocar novos pedidos.
+
+        while (true) {
+            SQSService.messageReader();
         }
     }
 }
-
--- src/main/java/br/com/sqs_ecommerce/services/SQSService.java
+++ src/main/java/br/com/sqs_ecommerce/services/SQSService.java
@@ -1,8 +1,9 @@
 package br.com.sqs_ecommerce.services;
 
-
 import java.time.LocalDateTime;
 import java.util.List;
+
+import javax.swing.MenuSelectionManager;
 
 import software.amazon.awssdk.services.sqs.SqsClient;
 import software.amazon.awssdk.services.sqs.model.DeleteMessageRequest;
@@ -16,91 +17,91 @@ import com.google.gson.Gson;
 
 import br.com.sqs_ecommerce.dto.InfMessage;
 
-
 public class SQSService {
-    public static void messageReader(){
-      
+
+    public static void messageReader() {
+
         GetQueueUrlRequest request = configAws.getUrl();
         SqsClient sqsClient = configAws.sqsClient();
+        GetQueueUrlResponse createResult = sqsClient.getQueueUrl(request);      
 
-        GetQueueUrlResponse createResult = sqsClient.getQueueUrl(request);
-        
         // TODO - refatorar em outra classe esta regra de negócio...
-        List<Message> messages = receiveMessages(sqsClient, createResult.queueUrl());      
-        for (Message mess : messages) {            
-            
+        List<Message> messages = receiveMessages(sqsClient, createResult.queueUrl());
+        
+        for (Message mess : messages) {
+
             String myId = "pedidos";
-            String transacao = "transacao";
-            String nf = "notaFiscal";
-            String estoque = "estoque";
-            String despacho = "despacho";
 
             String jsonMess = mess.body();
-                   
+
             InfMessage pedido = new Gson().fromJson(jsonMess, InfMessage.class);
             String to = pedido.getTo();
             String from = pedido.getFrom();
             LocalDateTime now = LocalDateTime.now();
-          
-            System.out.println("Mensagem: " + pedido.getFrom() + " - "
-            + pedido.getTo() + " - " 
-            + myId);
-            
-            // TODO - refatorar para um switch (if to.equals(myid), case transacao,nf,...)
-            if ( to.equals(myId) && from.equals(transacao)){                
-                System.out.println("[" + now + "][APROVADO TRANSACAO]" + " #Pedido: " + pedido.getNumPedido());                
-                deleteMessages(sqsClient, createResult.queueUrl(),  mess);                
-            } 
-            else if ( to.equals(myId) && from.equals(nf)){                
-                System.out.println("[" + now + "][NF EMITIDA]" + " #Pedido: " + pedido.getNumPedido());                
-                deleteMessages(sqsClient, createResult.queueUrl(),  mess);                
-            } 
-            else if ( to.equals(myId) && from.equals(estoque)){                
-                System.out.println("[" + now + "][ESTOQUE OK]" + " #Pedido: " + pedido.getNumPedido());                
-                deleteMessages(sqsClient, createResult.queueUrl(),  mess);                
-            } 
-            else if ( to.equals(myId) && from.equals(despacho)){                
-                System.out.println("[" + now + "][TRANSPORTADORA]" + " #Pedido: " + pedido.getNumPedido());                
-                deleteMessages(sqsClient, createResult.queueUrl(),  mess);                
-                //Só para gerar evento de tempo entre transporte e entrega.
-                pedido.setFrom(myId);
-                pedido.setTo(myId);
-                String jsonToPedido = new Gson().toJson(pedido);                 
-                sendMessage(sqsClient, createResult.queueUrl(), jsonToPedido);
-            } 
-            else if ( to.equals(myId) && from.equals(myId)){                
-                System.out.println("[" + now + "][ENTREGUE]" + " #Pedido: " + pedido.getNumPedido());                
-                deleteMessages(sqsClient, createResult.queueUrl(),  mess);                
-            }          
-        }   
+
+            //Monitorando as msg que estão sendo enviadas, so periodo de validacao.
+            System.out.println("Mensagem recebida, from: " + pedido.getFrom() + " - to: " + pedido.getTo());
+
+            if (to.equals(myId)) {
+                switch (from) {
+                    case "transacao":
+                        System.out.println("[" + now + "][APROVADO TRANSACAO]" + " #Pedido: " + pedido.getNumPedido());
+                        deleteMessages(sqsClient, createResult.queueUrl(), mess);
+                        break;
+                    case "notaFiscal":
+                        System.out.println("[" + now + "][NF EMITIDA]" + " #Pedido: " + pedido.getNumPedido());
+                        deleteMessages(sqsClient, createResult.queueUrl(), mess);
+                        break;
+                    case "estoque":
+                        System.out.println("[" + now + "][ESTOQUE OK]" + " #Pedido: " + pedido.getNumPedido());
+                        deleteMessages(sqsClient, createResult.queueUrl(), mess);
+                        break;
+                    case "despacho":
+                        System.out.println("[" + now + "][TRANSPORTADORA]" + " #Pedido: " + pedido.getNumPedido());
+                        deleteMessages(sqsClient, createResult.queueUrl(), mess);
+                        // Só para gerar evento de tempo entre transporte e entrega.
+                        pedido.setFrom(myId);
+                        pedido.setTo(myId);
+                        String jsonToPedido = new Gson().toJson(pedido);
+                        sendMessage(sqsClient, createResult.queueUrl(), jsonToPedido);
+                        break;
+                    case "pedidos":
+                        // este case é para simular entrega do pedido
+                        System.out.println("[" + now + "][ENTREGUE]" + " #Pedido: " + pedido.getNumPedido());
+                        deleteMessages(sqsClient, createResult.queueUrl(), mess);
+                        break;
+                    default:
+                        break;
+                }
+            }           
+        }
 
         sqsClient.close();
     }
 
-    public static  List<Message> receiveMessages(SqsClient sqsClient, String queueUrl) {
+    public static List<Message> receiveMessages(SqsClient sqsClient, String queueUrl) {        
         ReceiveMessageRequest receiveMessageRequest = ReceiveMessageRequest.builder()
-            .queueUrl(queueUrl)
-            .waitTimeSeconds(20)
-            .maxNumberOfMessages(5)
-            .build();
+                .queueUrl(queueUrl)
+                .waitTimeSeconds(20)
+                .maxNumberOfMessages(5)
+                .build();
         List<Message> messages = sqsClient.receiveMessage(receiveMessageRequest).messages();
         return messages;
     }
 
-    public static void deleteMessages(SqsClient sqsClient, String queueUrl,  Message message) {
-       
-            DeleteMessageRequest deleteMessageRequest = DeleteMessageRequest.builder()
+    public static void deleteMessages(SqsClient sqsClient, String queueUrl, Message message) {       
+        DeleteMessageRequest deleteMessageRequest = DeleteMessageRequest.builder()
                 .queueUrl(queueUrl)
                 .receiptHandle(message.receiptHandle())
                 .build();
-            sqsClient.deleteMessage(deleteMessageRequest);       
-   }
+        sqsClient.deleteMessage(deleteMessageRequest);
+    }
 
-   public static void sendMessage(SqsClient sqsClient, String queueUrl, String message) {
-    SendMessageRequest sendMsgRequest = SendMessageRequest.builder()
-        .queueUrl(queueUrl)       
-        .messageBody(message)
-        .build();
-    sqsClient.sendMessage(sendMsgRequest);
-}
+    public static void sendMessage(SqsClient sqsClient, String queueUrl, String message) {             
+        SendMessageRequest sendMsgRequest = SendMessageRequest.builder()
+                .queueUrl(queueUrl)
+                .messageBody(message)
+                .build();
+        sqsClient.sendMessage(sendMsgRequest);
+    }
 }
